ea664875054e25889db7ca0d67fa868013ae8f89
Solar M. <solar@lunar-dev.com>
2025-06-01 15:45:02 +0100

    Initial Commit

---
+++ src/main/kotlin/me/solar/infectionMinigame/InfectionMinigame.kt
@@ -0,0 +1,14 @@
+package me.solar.infectionMinigame
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class InfectionMinigame : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
