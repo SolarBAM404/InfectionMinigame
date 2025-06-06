00c26b22ef80e1a4fb2a759235c62054cf240e9f
Solar M. <solar@lunar-dev.com>
2025-06-06 02:17:58 +0100

    feat: added plugin getters for the better models

--- src/main/java/me/solar/infectionMinigame/InfectionMinigamePlugin.java
+++ src/main/java/me/solar/infectionMinigame/InfectionMinigamePlugin.java
@@ -1,12 +1,28 @@
 package me.solar.infectionMinigame;
 
+import kr.toxicity.model.api.BetterModel;
+import kr.toxicity.model.api.BetterModelPlugin;
+import kr.toxicity.model.api.manager.ModelManager;
+import lombok.Getter;
 import me.solar.apollo.apolloCore.ApolloPlugin;
+import me.solar.infectionMinigame.commands.AdminCommand;
 
 public class InfectionMinigamePlugin extends ApolloPlugin {
 
+    @Getter
+    private static InfectionMinigamePlugin instance;
+
+    @Getter
+    private static BetterModelPlugin betterModel;
+
+    @Getter
+    private static ModelManager modelManager;
+
     @Override
     public void start() {
-
+        AdminCommand.init();
+        betterModel = BetterModel.plugin();
+        modelManager = betterModel.modelManager();
     }
 
     @Override
