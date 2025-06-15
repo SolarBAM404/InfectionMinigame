ee3100e2ec2340f06a9d0cef2aa81fdf1d21a7e0
Solar M. <me@benjmainmacaulay.com>
2025-06-15 13:21:19 +0100

    feat: registered listener

--- src/main/java/me/solar/infectionMinigame/InfectionMinigamePlugin.java
+++ src/main/java/me/solar/infectionMinigame/InfectionMinigamePlugin.java
@@ -6,6 +6,7 @@ import kr.toxicity.model.api.manager.ModelManager;
 import lombok.Getter;
 import me.solar.apollo.apolloCore.ApolloPlugin;
 import me.solar.infectionMinigame.commands.AdminCommand;
+import me.solar.infectionMinigame.listeners.CustomMobEventsListener;
 
 public class InfectionMinigamePlugin extends ApolloPlugin {
 
@@ -24,6 +25,8 @@ public class InfectionMinigamePlugin extends ApolloPlugin {
         AdminCommand.init();
         betterModel = BetterModel.plugin();
         modelManager = betterModel.modelManager();
+
+        getServer().getPluginManager().registerEvents(new CustomMobEventsListener(), this);
     }
 
     @Override
