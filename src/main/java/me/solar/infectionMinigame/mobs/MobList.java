cb6092f645a877b87bb52e4cbe9d96384fee74fd
Solar M. <me@benjmainmacaulay.com>
2025-06-15 13:20:40 +0100

    feat: modified mob lists and add static get names method

--- src/main/java/me/solar/infectionMinigame/mobs/MobList.java
+++ src/main/java/me/solar/infectionMinigame/mobs/MobList.java
@@ -1,6 +1,8 @@
 package me.solar.infectionMinigame.mobs;
 
+import lombok.Getter;
 import me.solar.infectionMinigame.InfectionMinigamePlugin;
+import me.solar.infectionMinigame.mobs.types.DemonKnight;
 import me.solar.infectionMinigame.mobs.types.Speedy;
 import net.minecraft.world.level.Level;
 import org.bukkit.Location;
@@ -8,16 +10,19 @@ import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.EntityType;
 
 import javax.swing.text.html.parser.Entity;
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 public enum MobList {
 
-    DEMON_KNIGHT("Demon Knight", Speedy.class, 40.0f, 10.0f, 0.4f)
+    DEMON_KNIGHT("Demon Knight", DemonKnight.class, 40.0f, 10.0f, 0.4f),
+    SPEEDY("Speedy", Speedy.class, 20.0f, 5.0f, 0.3f)
     ;
 
 
+    @Getter
     private String name;
     private float health;
     private double damage;
@@ -45,5 +50,13 @@ public enum MobList {
         } catch (NoSuchMethodException | InvocationTargetException | InstantiationException | IllegalAccessException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static String[] getMobNames() {
+        String[] mobNames = new String[MobList.values().length];
+        for (int i = 0; i < MobList.values().length; i++) {
+            mobNames[i] = MobList.values()[i].getName();
+        }
+        return mobNames;
     }
 }
