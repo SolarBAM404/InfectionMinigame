4728bb8a3803955c7cd5a14e00ab12eaadab0866
Solar M. <me@benjmainmacaulay.com>
2025-10-10 02:29:46 +0100

    feat: add LockedDoor and abstract PickupItem classes for barricade mechanics

---
+++ src/main/java/me/solar/infectionMinigame/barricades/LockedDoor.java
@@ -0,0 +1,16 @@
+package me.solar.infectionMinigame.barricades;
+
+public class LockedDoor {
+
+    private int cost;
+    private int tier;
+
+    public void open() {
+
+    }
+
+    public void generateCost() {
+
+    }
+
+}
---
+++ src/main/java/me/solar/infectionMinigame/barricades/PickupItem.java
@@ -0,0 +1,9 @@
+package me.solar.infectionMinigame.barricades;
+
+public abstract class PickupItem {
+
+    public abstract void onPickup();
+
+    public abstract void cost();
+
+}
