c5b35dff5ca84d28408a2586912d7a2847cba865
Solar M. <me@benjmainmacaulay.com>
2025-10-14 17:01:03 +0100

    feat: extend LockedDoor class to inherit from Barricade and implement region handling

--- src/main/java/me/solar/infectionMinigame/barricades/LockedDoor.java
+++ src/main/java/me/solar/infectionMinigame/barricades/LockedDoor.java
@@ -1,9 +1,19 @@
 package me.solar.infectionMinigame.barricades;
 
-public class LockedDoor {
+import me.solar.apolloLibrary.world.CuboidRegion;
+import me.solar.apolloLibrary.world.Region;
+import org.bukkit.Location;
+
+public class LockedDoor extends Barricade {
+
+    private final Region region;
 
     private int cost;
     private int tier;
+
+    public LockedDoor(Region region) {
+        this.region = region;
+    }
 
     public void open() {
 
@@ -12,5 +22,16 @@ public class LockedDoor {
     public void generateCost() {
 
     }
+
+    @Override
+    public void spawn() {
+        
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
 
 }
