b81489ac6aee60a6f60dda10d5222df65aef6d9c
Solar M. <me@benjmainmacaulay.com>
2025-11-02 23:56:16 +0000

    feat: add new admin commands for map creation and editing functionality

--- src/main/java/me/solar/infectionMinigame/commands/AdminCommand.java
+++ src/main/java/me/solar/infectionMinigame/commands/AdminCommand.java
@@ -1,12 +1,10 @@
 package me.solar.infectionMinigame.commands;
 
+import com.magmaguy.freeminecraftmodels.customentity.DynamicEntity;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
-import kr.toxicity.model.api.data.renderer.ModelRenderer;
-import kr.toxicity.model.api.tracker.EntityTracker;
 import me.solar.apolloLibrary.utils.Common;
 import me.solar.apolloLibrary.world.CuboidRegion;
 import me.solar.infectionMinigame.InfectionMinigamePlugin;
@@ -14,14 +12,16 @@ import me.solar.infectionMinigame.barricades.LockedDoor;
 import me.solar.infectionMinigame.barricades.PickupItem;
 import me.solar.infectionMinigame.barricades.RepairableBarricade;
 import me.solar.infectionMinigame.barricades.items.DroppableItems;
+import me.solar.infectionMinigame.data.GameMap;
+import me.solar.infectionMinigame.dialogs.Dialogs;
+import me.solar.infectionMinigame.menus.MapMenu;
 import me.solar.infectionMinigame.mobs.MobList;
+import me.solar.infectionMinigame.tools.ArenaSelectionTool;
 import org.bukkit.Location;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
-import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AdminCommand {
 
@@ -29,17 +29,20 @@ public class AdminCommand {
         LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("admin");
         node.then(reloadCmd());
         node.then(testModelMobSpawningCmd());
-        node.then(testCustomMobModelSpawningCmd());
+        node.then(testAnimation());
         node.then(testBarrierCmd());
         node.then(testItemDropCmd());
         node.then(testLockedDoorCmd());
+        node.then(createNewMap());
+        node.then(editorMenu());
+        node.then(mapsMenu());
         return node;
     }
 
     public static LiteralArgumentBuilder<CommandSourceStack> reloadCmd() {
         LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("reload");
         node.executes((ctx) -> {
-            InfectionMinigamePlugin.getInstance().reloadConfig();
+            InfectionMinigamePlugin.getInstance().reload();
             Common.tell(ctx.getSource().getExecutor(), "<green>Config reloaded!");
             return 1;
         });
@@ -49,44 +52,35 @@ public class AdminCommand {
     public static LiteralArgumentBuilder<CommandSourceStack> testModelMobSpawningCmd() {
         LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("testmodelmob");
         node.executes(context -> {
-            ModelRenderer demonKnight = InfectionMinigamePlugin.getBetterModel().modelManager().renderer("demon_knight");
-            if (demonKnight == null) {
-                Common.tell((Player) context.getSource(), "<red>Demon Knight model not found!");
-                return 0; // Return failure
-            }
-            Entity entity = context.getSource().getExecutor().getWorld().spawnEntity(context.getSource().getLocation(), EntityType.ZOMBIE);
-            EntityTracker tracker = demonKnight.create(entity);
-            tracker.spawn((Player) context.getSource().getExecutor());
+            MobList.SPEEDY.spawn(context.getSource().getLocation());
             return 1; // Return success
         });
         return node;
     }
 
-    public static LiteralArgumentBuilder<CommandSourceStack> testCustomMobModelSpawningCmd() {
-        return Commands.literal("testcustommob")
-                .then(Commands.argument("name", StringArgumentType.greedyString())
-                        .suggests((context, builder) -> {
-                            Arrays.stream(MobList.getMobNames()).filter(name -> name.toLowerCase().startsWith(builder.getRemaining().toLowerCase()))
-                                    .forEach(builder::suggest);
-                            return builder.buildFuture();
-                        })
-                        .executes(context -> {
-                            String mobName = StringArgumentType.getString(context, "name");
-                            MobList mob = Arrays.stream(MobList.values())
-                                    .filter(m -> m.getName().equalsIgnoreCase(mobName))
-                                    .findFirst()
-                                    .orElse(null);
-                            if (mob == null) {
-                                Common.tell((Player) context.getSource(), "<red>Mob not found: " + mobName);
-                                return 0; // Return failure
-                            }
-                            mob.spawn(context.getSource().getLocation());
-                            return 1; // Return success
-                        }))
+    public static LiteralArgumentBuilder<CommandSourceStack> testAnimation() {
+        return Commands.literal("testAnimation")
                 .executes(context -> {
-                    Common.tell((Player) context.getSource(), "<red>Usage: /admin testcustommob <mob_name>");
-                    Common.tell((Player) context.getSource(), "<red>Available mobs: " + String.join(", ", MobList.getMobNames()));
-                    return 0; // Return failure
+                    if (context.getSource().getExecutor() == null || !(context.getSource().getExecutor() instanceof Player player)) {
+                        return 0;
+                    }
+
+
+                    AtomicReference<DynamicEntity> entity = new AtomicReference<>();
+                    player.getLocation().getWorld().getNearbyEntities(player.getLocation(), 5, 5, 5).forEach(e -> {
+                        if (DynamicEntity.isDynamicEntity(e)) {
+                            entity.set(DynamicEntity.getDynamicEntity(e));
+                        }
+                    });
+
+                    if (entity.get() == null) {
+                        Common.tell(player, "<red>No entity found!");
+                        return 0;
+                    }
+
+                    DynamicEntity dynamicEntity = entity.get();
+                    dynamicEntity.playAnimation("attack", false, true);
+                    return 1;
                 });
     }
 
@@ -145,6 +139,61 @@ public class AdminCommand {
         node.executes(context -> {
             LockedDoor door = new LockedDoor(new CuboidRegion(context.getSource().getLocation(), context.getSource().getLocation().add(2, 2, 0)));
             door.spawn();
+            return 1;
+        });
+        return node;
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> createNewMap() {
+        LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("createmap");
+        node.executes(context -> {
+            Entity executor = context.getSource().getExecutor();
+            if (!(executor instanceof Player player)) {
+                Common.tell(context.getSource().getExecutor(), "<red>You must be a player to use this command!");
+                return 0;
+            }
+
+            ArenaSelectionTool.getInstance().giveIfHasnt(player);
+            Common.tell(player, "<green>You may now start setting up your map!");
+            return 1;
+        });
+        return node;
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> editorMenu() {
+        LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("editor");
+        node.executes(context -> {
+            if (context.getSource().getExecutor() == null) {
+                throw new IllegalArgumentException("CommandSourceStack cannot be null!");
+            }
+
+            if (!(context.getSource().getExecutor() instanceof Player player)) {
+                Common.tell(context.getSource().getExecutor(), "<red>You must be a player to use this command!");
+                return 0;
+            }
+
+            GameMap editorMap = GameMap.getEditorCache().get(context.getSource().getExecutor().getUniqueId());
+            if (editorMap == null) {
+                Common.tell(context.getSource().getExecutor(), "<red>You must be in an editor menu to use this command!");
+                return 0;
+            }
+
+//            new EditorMenu(editorMap).open((Player) context.getSource().getExecutor());
+            player.showDialog(Dialogs.EditorDialog(editorMap));
+            return 1;
+        });
+        return node;
+    }
+
+    public static LiteralArgumentBuilder<CommandSourceStack> mapsMenu() {
+        LiteralArgumentBuilder<CommandSourceStack> node = LiteralArgumentBuilder.literal("maps");
+        node.executes(context -> {
+            if (context.getSource().getExecutor() == null || !(context.getSource().getExecutor() instanceof Player player)) {
+                Common.tell(context.getSource().getExecutor(), "<red>You must be a player to use this command!");
+                return 0;
+            }
+
+            MapMenu.getInstance().open(player);
             return 1;
         });
         return node;
